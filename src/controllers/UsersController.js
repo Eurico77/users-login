200611dc2199272d247649f4462309ebd19da40d
Eurico77 <euricomagalhaes61@gmail.com>
2020-04-02 19:55:19 -0300

    finish api

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,11 +1,9 @@
 const connection = require("../database/connection");
 
-
 module.exports = {
   async store(req, res) {
+    const user = ({ name, surname, email } = req.body);
     
-    const user = { name, surname , email } = req.body;
-
     await connection("users").insert({
       name,
       surname,
@@ -14,12 +12,11 @@ module.exports = {
 
     return res.json({ user });
   },
-  
-  async index(req, res) {
-    routes.get("/employees", async (req, res) => {
-      const users = await connection("users").select("*");
 
-      return res.json(user);
-    });
-  }
+  async index(req, res) {
+    const users = await connection("users").select("*");
+
+    return res.json(users);
+  },
+
 };
